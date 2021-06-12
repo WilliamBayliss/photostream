10fe2bf344355c097860872bd5b5989197a5964a
williambayliss <wmbayliss@gmail.com>
2021-06-12 11:31:49 -0400

    Use flickr api to call photos if flickr user id is entered in search bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
+require 'flickr'
 class StaticPagesController < ApplicationController
   def index
+    if params[:flickr_user_id]
+
+      flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_SHARED_SECRET'])
+      @photostream = flickr.people.getPhotos :user_id => params[:flickr_user_id]
+    end
   end
 end
